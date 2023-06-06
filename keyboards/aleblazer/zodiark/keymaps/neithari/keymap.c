da1a318161be7bb849f01145436fbab2379f8db9
Manuel Etspüler <etspueler@phoenix-pacs.de>
2023-06-06 08:52:11 +0200

    Fixed some minor issues

--- keyboards/aleblazer/zodiark/keymaps/neithari/keymap.c
+++ keyboards/aleblazer/zodiark/keymaps/neithari/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = LAYOUT(
       QK_GESC,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-      KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  LSFT(KC_8),             LSFT(KC_9),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+      KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  ALGR(KC_7),             ALGR(KC_0),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
       CW_TOGG,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_RBRC,                   KC_EQL,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       LM(_SHIFT,MOD_LSFT),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_DEL, KC_MUTE,  RGB_TOG, KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
       KC_LCTL,            KC_LGUI, KC_LALT, KC_NUBS, KC_LOWER,KC_SPC,   KC_ENT,               KC_ENT,     KC_SPC,  KC_RAISE,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
@@ -75,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 	[_SHIFT] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, ALGR(KC_7),                ALGR(KC_0), _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,                   _______,    _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______,      _______,     _______, _______,        _______,      _______, _______, _______, _______, _______
+      _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, KC_8,                      KC_9,    _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______,      _______,     _______, _______,        _______,   _______, _______, _______, _______, _______
       ),
 
 /* RAISE
@@ -119,10 +119,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 	[_LOWER] = LAYOUT(
-      KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,                                       KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  KC_F12,
-      _______, KC_P7,   KC_P7,  KC_P8,  KC_P9,  KC_PMNS, ALGR(8),                   ALGR(9), KC_PSLS, KC_P7,   KC_P8, KC_P9,   KC_PMNS, KC_NUM, 
-      KC_CAPS, KC_P4,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, KC_F14,                    KC_F15,  KC_PAST, KC_P4,   KC_P5, KC_P6,   KC_PPLS, KC_F17, 
-      _______, KC_F13,  KC_P1,  KC_P2,  KC_P3,  KC_COMM, _______, _______,  _______,_______, KC_F16,  KC_P1,   KC_P2, KC_P3,   KC_COMM, KC_F18, 
+      KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,                                          KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  KC_F12,
+      _______, KC_P7,   KC_P7,  KC_P8,  KC_P9,  KC_PMNS, ALGR(KC_8),                ALGR(KC_9), KC_PSLS, KC_P7,   KC_P8, KC_P9,   KC_PMNS, KC_NUM, 
+      KC_CAPS, KC_P4,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, KC_F14,                    KC_F15,     KC_PAST, KC_P4,   KC_P5, KC_P6,   KC_PPLS, KC_F17, 
+      _______, KC_F13,  KC_P1,  KC_P2,  KC_P3,  KC_COMM, _______, _______,  _______,_______,    KC_F16,  KC_P1,   KC_P2, KC_P3,   KC_COMM, KC_F18, 
       _______, _______, KC_P0,  KC_PDOT,_______,     _______,     _______,  _______,      _______,    _______, KC_P0, KC_PDOT, KC_PENT, KC_F19
       ),
 
@@ -189,6 +189,9 @@ static void print_status_narrow(void) {
         case _COLEMAK:
         case _QWERTY:
             oled_write_P(PSTR("Base\n"), false);
+            break;
+        case _SHIFT:
+            oled_write_P(PSTR("Shift\n"), false);
             break;
         case _RAISE:
             oled_write_P(PSTR("Raise"), false);
