7494d9a427a61a64cb37890a682c345d2ce7966d
Manuel Etspüler <etschi3@yahoo.de>
2025-04-23 00:46:48 +0200

    switched keys

--- keyboards/aleblazer/zodiarkpi/keymaps/neithari/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/neithari/keymap.c
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [_QWERTY] = LAYOUT(
         KC_ESC,             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                         KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                         KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-        CW_TOGG,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_RBRC, KC_MUTE,    RGB_TOG, KC_NUHS, KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        LM(_SHIFT,MOD_LSFT),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_BSPC, ALGR(KC_0),ALGR(KC_7),KC_DEL, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_EQL),
+        CW_TOGG,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  ALGR(KC_7), KC_MUTE,RGB_TOG,ALGR(KC_0),KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        LM(_SHIFT,MOD_LSFT),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_RBRC,KC_BSPC,      KC_DEL, KC_NUHS, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_EQL),
         KC_LCTL,            KC_LGUI, KC_NUBS, KC_LALT, KC_LOWER,   KC_SPC,      KC_ENT,        KC_ENT,      KC_SPC,   KC_RAISE,KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
       ),
   
@@ -77,8 +77,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [_SHIFT] = LAYOUT(
         KC_GRV,  _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_DEL,     KC_8,    KC_9, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_8, _______, _______,       KC_9, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,  KC_DEL, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,      _______,     _______, _______,      _______,     _______, _______, _______, _______, _______
       ),
   
@@ -99,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [_RAISE] = LAYOUT(
         KC_GRV,  _______, _______, _______, _______,  _______,                                       _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,  _______,                                       _______, _______, KC_UP,   _______, _______, _______,
-        KC_CAPS, KC_PGDN, KC_PGUP, KC_HOME, KC_END,   _______, _______, _______, _______, _______,   _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, _______,
-        _______, _______, _______, _______, _______,  _______, _______,KC_NUBS,S(KC_NUBS),_______,   _______, KC_F10,  KC_F11,  KC_F5,   _______, _______,
+        KC_CAPS, KC_PGDN, KC_PGUP, KC_HOME, KC_END,   _______, KC_NUBS, _______, _______, S(KC_NUBS),_______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, _______,
+        _______, _______, _______, _______, _______,  _______, _______,_______,   _______,_______,   _______, KC_F10,  KC_F11,  KC_F5,   _______, _______,
         _______, _______, _______, _______, KC_ADJUST,     _______,     _______, _______,       _______,      _______, _______, _______, _______, _______
       ),
   
@@ -121,9 +121,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [_LOWER] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,                                          KC_F6,   KC_F7,    KC_F8, KC_F9,   KC_F10,  KC_F11,
         _______, KC_P7,   KC_P7,  KC_P8,  KC_P9,  KC_PMNS,                                        KC_PSLS, KC_P7,    KC_P8, KC_P9,   KC_PMNS, KC_F12, 
-        KC_CAPS, KC_P4,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, KC_F15,  _______,  _______,    KC_F16, KC_PAST, KC_P4,    KC_P5, KC_P6,   KC_PPLS, KC_F13, 
-        _______, KC_UNDO, KC_P1,  KC_P2,  KC_P3,  KC_COMM, _______,ALGR(KC_8),ALGR(KC_9),_______, KC_NUM,  KC_P1,    KC_P2, KC_P3,   KC_DOT,  KC_F14, 
-        _______, _______, KC_P0,  KC_PDOT,_______,     _______,     _______,  _______,       _______,      KC_ADJUST,KC_P0, KC_PDOT, KC_PENT, KC_PEQL
+        KC_CAPS, KC_P4,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, ALGR(KC_8),_______,_______,ALGR(KC_9), KC_PAST, KC_P4,    KC_P5, KC_P6,   KC_PPLS, KC_F13, 
+        _______, KC_UNDO, KC_P1,  KC_P2,  KC_P3,  KC_COMM, KC_F15, _______,  _______, KC_F16,     KC_NUM,  KC_P1,    KC_P2, KC_P3,   KC_DOT,  KC_F14, 
+        _______, _______, KC_P0,  KC_PDOT,_______,     _______,    _______,  _______,       _______,       KC_ADJUST,KC_P0, KC_PDOT, KC_PENT, KC_PEQL
       ),
   
   /* ADJUST
